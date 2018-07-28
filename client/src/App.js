f496c3d88ecd06d1752847cb726ab8d625a2b917
wcywin <wcywin@gmail.com>
2018-07-28 12:46:40 +0200

    react front-end done

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,78 @@
 import React, { Component } from 'react';
+import { Form, FormGroup, Label, Input, Button } from 'reactstrap';
+import axios from 'axios';
 import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
 
+
 class App extends Component {
+    constructor() {
+        super();
+
+        this.state = {
+            name: '',
+            email: '',
+            message: ''
+        };
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange = (e) => {
+        this.setState({
+            [e.target.name]: e.target.value
+        });
+    };
+
+    async handleSubmit(e) {
+        e.preventDefault();
+
+        const { name, email, message } = this.state;
+
+        const form = await axios.post('/api/form', {
+            name,
+            email,
+            message
+        });
+    }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">React Contact Form</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+          <Form className="App-form" onSubmit={this.handleSubmit}>
+              <FormGroup>
+                  <Label for="name">Name</Label>
+                  <Input
+                    type="text"
+                    name="name"
+                    onChange={this.handleChange}
+                  />
+              </FormGroup>
+              <FormGroup>
+                  <Label for="email">Email:</Label>
+                  <Input
+                      type="email"
+                      name="email"
+                      onChange={this.handleChange}
+                  />
+              </FormGroup>
+              <FormGroup>
+                  <Label for="message">Message:</Label>
+                  <Input
+                      type="textarea"
+                      name="message"
+                      onChange={this.handleChange}
+                  />
+              </FormGroup>
+
+              <Button>Submit</Button>
+          </Form>
       </div>
     );
   }
